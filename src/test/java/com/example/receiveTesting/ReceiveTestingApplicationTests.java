300b42fea841af7e3b361e27046644119177f753
kristinau09 <kristinau09@gmail.com>
2021-02-13 15:12:05 -0600

    Receiving messages from the message broker

---
+++ src/test/java/com/example/receiveTesting/ReceiveTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receiveTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiveTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
